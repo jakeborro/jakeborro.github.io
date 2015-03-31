2ee49d1e792b484b4f96bef8af6d3da085e0c0f1
Jake Borro <jakegborro@gmail.com>
2015-03-31 16:09:47 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -15,3 +15,20 @@
 	};
 
 })();
+
+function init() {
+    window.addEventListener('scroll', function(e){
+        var distanceY = window.pageYOffset || document.documentElement.scrollTop,
+            shrinkOn = 500,
+            header = document.querySelector("header");
+        if (distanceY > shrinkOn) {
+            classie.add(header,"smaller");
+        } else {
+            if (classie.has(header,"smaller")) {
+                classie.remove(header,"smaller");
+            }
+        }
+    });
+}
+window.onload = init();
+
