52eecf02bd70433809ae11850c2959fb86544d9e
Jake Borro <jakegborro@gmail.com>
2015-04-01 17:57:24 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -16,19 +16,49 @@
 
 })();
 
-(function init() {
-    window.addEventListener('scroll', function(e){
-        var distanceY = window.pageYOffset || document.documentElement.scrollTop,
-            shrinkOn = 500,
-            header = document.querySelector("header");
-        if (distanceY > shrinkOn) {
-            classie.add(header,"smaller");
-        } else {
-            if (classie.has(header,"smaller")) {
-                classie.remove(header,"smaller");
-            }
+//Header
+
+;(function(document, window, index) {
+    'use strict';
+
+    var elSelector = '.header',
+        element = document.querySelector(elSelector);
+
+    if (!element) return true;
+
+    var elHeight = 0,
+        elTop = 0,
+        dHeight = 0,
+        wHeight = 0,
+        wScrollCurrent = 0,
+        wScrollBefore = 0,
+        wScrollDiff = 0;
+
+    window.addEventListener('scroll', function() {
+        elHeight = element.offsetHeight;
+        dHeight = document.body.offsetHeight;
+        wHeight = window.innerHeight;
+        wScrollCurrent = window.pageYOffset;
+        wScrollDiff = wScrollBefore - wScrollCurrent;
+        elTop = parseInt(window.getComputedStyle(element).getPropertyValue('top')) + wScrollDiff;
+
+        if (wScrollCurrent <= 0)
+            element.style.top = '0px';
+
+        else if (wScrollDiff > 0)
+            element.style.top = (elTop > 0 ? 0 : elTop) + 'px';
+
+        else if (wScrollDiff < 0) {
+            if (wScrollCurrent + wHeight >= dHeight - elHeight)
+                element.style.top = ((elTop = wScrollCurrent + wHeight - dHeight) < 0 ? elTop : 0) + 'px';
+
+            else
+                element.style.top = (Math.abs(elTop) > elHeight ? -elHeight : elTop) + 'px';
         }
+
+        wScrollBefore = wScrollCurrent;
     });
-}
-window.onload = init();
+
+}(document, window, 0));
+
 
