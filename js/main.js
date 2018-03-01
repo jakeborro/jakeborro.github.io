304d1ec7d9a6f905b2f79210d10164ac100d2c0a
Jake <jakegborro@gmail.com>
2018-03-01 11:15:53 -0700

    Revert "Delete main.js"
    This reverts commit 09c23ae1183a8f09b179055b17ea675ecb5273fd.

---
+++ js/main.js
@@ -0,0 +1,62 @@
+(function(){
+
+	var parallax = document.querySelectorAll(".parallax"),
+	    speed = 0.5;
+
+	window.onscroll = function(){
+		[].slice.call(parallax).forEach(function(el,i){
+
+			  var windowYOffset = window.pageYOffset,
+				elBackgroundPos = "50% " + (windowYOffset * speed) + "px";
+
+			el.style.backgroundPosition = elBackgroundPos;	
+
+		});
+	};
+
+})();
+
+//Header
+
+;(function(document, window, index) {
+    'use strict';
+
+    var elSelector = '.header',
+        element = document.querySelector(elSelector);
+
+    if (!element) return true;
+
+    var elHeight = 0,
+        elTop = 0,
+        dHeight = 0,
+        wHeight = 0,
+        wScrollCurrent = 0,
+        wScrollBefore = 0,
+        wScrollDiff = 0;
+
+    window.addEventListener('scroll', function() {
+        elHeight = element.offsetHeight;
+        dHeight = document.body.offsetHeight;
+        wHeight = window.innerHeight;
+        wScrollCurrent = window.pageYOffset;
+        wScrollDiff = wScrollBefore - wScrollCurrent;
+        elTop = parseInt(window.getComputedStyle(element).getPropertyValue('top')) + wScrollDiff;
+
+        if (wScrollCurrent <= 0)
+            element.style.top = '0px';
+
+        else if (wScrollDiff > 0)
+            element.style.top = (elTop > 0 ? 0 : elTop) + 'px';
+
+        else if (wScrollDiff < 0) {
+            if (wScrollCurrent + wHeight >= dHeight - elHeight)
+                element.style.top = ((elTop = wScrollCurrent + wHeight - dHeight) < 0 ? elTop : 0) + 'px';
+
+            else
+                element.style.top = (Math.abs(elTop) > elHeight ? -elHeight : elTop) + 'px';
+        }
+
+        wScrollBefore = wScrollCurrent;
+    });
+
+}(document, window, 0));
